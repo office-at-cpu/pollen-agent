db2fea4cf9f6281a7ada041a14591436427a09d1
office-at-cpu <office-at@as-advertising.info>
2026-02-02 20:23:31 +0100

    Define process.env in Vite configuration
    Add process.env definition for browser context

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Stellt sicher, dass process.env im Browser-Kontext definiert ist
+    'process.env': {}
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
