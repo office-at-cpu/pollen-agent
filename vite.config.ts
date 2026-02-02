233d1b2a64a1c7e6e94fb3c0a98cbf0e382d5be3
office-at-cpu <office-at@as-advertising.info>
2026-02-02 20:34:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Stellt sicher, dass process.env im Browser-Kontext definiert ist
+    // Ersetzt process.env.API_KEY durch den tatsächlichen Wert während des Builds
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env': {}
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: './index.html',
